22af4b4b154f614e09911bd9d6284147e07a7755
Marecarenbg <stajexexe@hotmail.com>
2018-03-06 12:45:34 +0100

    compose defined?

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import thunk from 'redux-thunk';
@@ -12,7 +12,6 @@ export default () => {
       filters: filtersReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
